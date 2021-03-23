a765fba88ced662dbd72a556d6e69ccdb40144a5
robertwagar <robertwagar@gmail.com>
2021-03-23 08:35:46 +0100

    fixed api key token issue... ooopsy

--- src/components/Trello.js
+++ src/components/Trello.js
@@ -2,33 +2,16 @@
 import useFetch from './Fetch/useFetch';
 import Loading from './Loading/Loading'
 
-const KEY = 'cd9fc14d6c8d2b4dff0208735385410f'
-
-const TOKEN = 'de2cc82232b0b3c1e867b5b21deb88eacb4522851ff0b3e6535db384fe0ed3f8'
+import { KEY, TOKEN } from "../constants"
 
 const Trello = () => {
 
     
     const { data: data, isLoading, error } = useFetch(`https://api.trello.com/1/members/me/boards?key=${KEY}&token=${TOKEN}`);
     const board = useFetch(`https://api.trello.com/1/boards/603cc2d706938e4522a2db36?key=${KEY}&token=${TOKEN}`); 
-   /*  const board = useFetch(`https://api.trello.com/1/boards/603cc2d706938e4522a2db36?key=cd9fc14d6c8d2b4dff0208735385410f&token=de2cc82232b0b3c1e867b5b21deb88eacb4522851ff0b3e6535db384fe0ed3f8`);  */
+  
 
     console.log(board)
-
-
-    /* const getBoard = () => {
-        fetch(`https://api.trello.com/1/boards/603cc2d706938e4522a2db36?key=${KEY}&token=${TOKEN}`)
-          .then(res => {
-            return res.json()
-          })
-          .then(json => {
-            setBoard(json)
-          })
-      } */
-    
-      /* useFetch(`https://api.trello.com/1/boards/603cc2d706938e4522a2db36?key=${KEY}&token=${TOKEN}`) */
-    
-    
     
 
     return ( 
