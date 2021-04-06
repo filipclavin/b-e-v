1fc95d1590c3b23eb60f6e0b1f8db120a40a410d
SjostromJesper <j.sjostrom@live.se>
2021-04-06 14:44:16 +0200

    added more trello functions

--- src/utils/trello.js
+++ src/utils/trello.js
@@ -1,7 +1,5 @@
 const key = '6d3cbfc77587e09c39ce716ffc8d43f2';
 const token = '0dcb80152cd2afa6dcdb96e1d333d613cfb37f22b68cb780199513488a6781f5';
-const listId = '603e3f1761dcdc39c5540536';
-const cardName = 'hello world!';
 
 export const getTrelloBoard = (boardId) => {
 
@@ -41,6 +39,39 @@ export const postNewCard = (list, text, user) => {
 export const getBoardLists = (boardId) => {
     fetch(`https://api.trello.com/1/boards/${boardId}/lists?key=${key}&token=${token}`, {
         method: 'GET'
+    })
+        .then(response => {
+            console.log(
+                `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        })
+        .then(text => console.log(text))
+        .catch(err => console.error(err));
+}
+
+export const getSelectedList = (listId) => {
+    fetch(`https://api.trello.com/1/lists/${listId}/cards?key=${key}&token=${token}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            console.log(
+                `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        })
+        .then(text => console.log(text))
+        .catch(err => console.error(err));
+}
+
+
+const memberId = '5fb28148005793058ac2ba8d' //robbans id
+export const getMemberFromId = (memberId) => {
+    fetch(`https://api.trello.com/1/members/${memberId}?key=${key}&token=${token}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
     })
         .then(response => {
             console.log(
