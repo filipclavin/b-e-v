3d0a851c1fe8b6d3e071aa88b52d4b76ea14fcf9
robonexx <robertwagar@gmail.com>
2021-04-14 17:43:13 +0200

    fixed a little in the overview, styled repo list render and buttons fixed some with header api avatar and name

--- src/Pages/Overview.js
+++ src/Pages/Overview.js
@@ -4,6 +4,75 @@ import { getRepos } from "../utils/github"
 import { getLanguageData } from "../utils/github"
 import Dashboard from "./Dashboard"
 import { githubLogOut, getCurrentUser } from "../utils/firebase"
+import Header from '../components/Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+margin: 0;
+display: flex;
+flex-direction: column;
+width: 100vw;
+min-height: 100vh;
+background: radial-gradient(ellipse at bottom, rgb(31, 31, 48), #343E59);
+color: #e1e2e3;
+
+&>h1 {
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: 0;
+}
+&>h3 {
+    margin-top: 0;
+    text-align: center;
+    font-size: 1.6rem;
+}
+`;
+
+const Buttons = styled.div`
+    position: absolute;
+    width: fit-content;
+    top: 2%;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 10;
+
+&>button {
+    position: relative;
+    padding: 8px 12px;
+    outline: none;
+    width: fit-content;
+    z-index: 10;
+    margin-left: 1rem;
+}
+`;
+
+const RepoList = styled.div`
+margin: 0 auto;
+padding: 1rem;
+width: 90%;
+height: 100%;
+display: flex;
+flex-flow: row wrap;
+justify-content: center;
+box-shadow: 2px 3px 6px rgba(0,0,0,0.4);
+
+`;
+
+const RepoPreview = styled.div`
+    width: 20%;
+    padding: 0.5rem 1rem;
+    margin: 1rem 1rem;
+    border-bottom: 1px solid #e1e2e3;
+    border-right: 1px solid #e1e2e3;
+    text-transform: uppercase;
+
+    &>h3 {
+        text-align: center;
+    }
+
+&:hover {
+    box-shadow: 2px 3px 6px rgba(223, 217, 217, 0.603);
+`;
 
 const Overview = () => {
 
@@ -20,21 +89,37 @@ const Overview = () => {
     if (repos) console.log(repos);
 
     return (
-        <>
-            <button onClick={githubLogOut}>Log out</button>
+        <>           
             {
                 selectedRepo ?
                     <>
+                        <Dashboard repo={selectedRepo}></Dashboard>
+                        <Buttons>
+                        <button onClick={githubLogOut}>Log out</button>
                         <button onClick={() => setSelectedRepo()}>Back to Overview</button>
-                        <Dashboard repo={selectedRepo} />
-                    </> :
+                        </Buttons>
+                    </> : 
                     repos ?
+                    <Wrapper>
+                        <h1>Repository Overview</h1>
+                        <h3>Select a repository {`&`} continue to dashboard or log out</h3>
+                        <Buttons>
+                        <button onClick={githubLogOut}>Log out</button>
+                        </Buttons>
+                        <RepoList >
+                        {
                         repos.map(repo => {
                             return (
-                                <h1 onClick={() => setSelectedRepo(repo.url)}>{repo.name}</h1>
+                                    <RepoPreview onClick={() => setSelectedRepo(repo.url)}>
+                                <h3>{repo.name}</h3>
+                                    </RepoPreview>
+                                
                             )
-                        }) :
-                        null
+                        }) 
+                        } 
+                        </RepoList>
+                    </Wrapper>
+                         : null
             }
         </>
     )
